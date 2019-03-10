d22db7f0ec9dfbce888d380cbb2bb56b0165bc50
MathieuBuisson <matthieu.buisson@gmail.com>
2019-03-10 15:48:28 +0000

    Add ResourceGroupsHelper and unit tests for it

---
+++ src/ArmEval.Core/ResourceGroupsHelper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.Management.ResourceManager;
+using Microsoft.Azure.Management.ResourceManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArmEval.Core
+{
+    public static class ResourceGroupsHelper
+    {
+        public static void Create(IResourceManagementClient client, string resourceGroupName, string location)
+        {
+            var resourceGroup = new ResourceGroup();
+            resourceGroup.Location = location;
+            client.ResourceGroups.CreateOrUpdate(resourceGroupName, resourceGroup);
+
+        }
+
+        public static void CreateIfNotExists(IResourceManagementClient client, string resourceGroupName, string location)
+        {
+            if (!client.ResourceGroups.CheckExistence(resourceGroupName))
+            {
+                Console.WriteLine(string.Format("Creating resource group '{0}' in '{1}'", resourceGroupName, location));
+                Create(client, resourceGroupName, location);
+            }
+            else
+            {
+                Console.WriteLine(string.Format("Using existing resource group '{0}'", resourceGroupName));
+            }
+        }
+
+        public static void DeleteIfExists(IResourceManagementClient client, string resourceGroupName)
+        {
+            if (client.ResourceGroups.CheckExistence(resourceGroupName))
+            {
+                Console.WriteLine(string.Format("Deleting resource group '{0}'", resourceGroupName));
+                client.ResourceGroups.Delete(resourceGroupName);
+            }
+        }
+    }
+}
---
+++ tests/ArmEval.Core.Tests/ResourceGroupsHelperTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Azure.Management.ResourceManager;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ArmEval.Core.Tests
+{
+    public class ResourceGroupsHelperTests
+    {
+        private readonly string resourceGroupName = "ArmEvalTests-rg";
+        private readonly string location = "North Europe";
+
+        [Fact]
+        public void CreateIfNotExists_ResourceGroupDoesNotExist_CreatesIt()
+        {
+            using (var client = new ArmClient().Create())
+            {
+                ResourceGroupsHelper.CreateIfNotExists(client, resourceGroupName, location);
+
+                Assert.True(client.ResourceGroups.CheckExistence(resourceGroupName));
+            }
+        }
+
+        [Fact]
+        public void CreateIfNotExists_ResourceGroupExists_DoesNotThrow()
+        {
+            using (var client = new ArmClient().Create())
+            {
+                ResourceGroupsHelper.CreateIfNotExists(client, resourceGroupName, location);
+                Action act = () => { ResourceGroupsHelper.CreateIfNotExists(client, resourceGroupName, location); };
+                var ex = Record.Exception(act);
+
+                Assert.Null(ex);
+            }
+        }
+
+        [Fact]
+        public void DeleteIfExists()
+        {
+            using (var client = new ArmClient().Create())
+            {
+                var existenceBefore = client.ResourceGroups.CheckExistence(resourceGroupName);
+                ResourceGroupsHelper.DeleteIfExists(client, resourceGroupName);
+                var existenceAfter = client.ResourceGroups.CheckExistence(resourceGroupName);
+
+                Assert.True(existenceBefore);
+                Assert.False(existenceAfter);
+            }
+        }
+    }
+}
